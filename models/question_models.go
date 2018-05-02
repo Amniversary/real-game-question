d8b7480f5ae4b4dc69fd2ed60daf158836d8ae3b
Gavean <623006297@qq.com>
2018-05-02 16:20:19 +0800

    update

--- models/question_models.go
+++ models/question_models.go
@@ -88,14 +88,14 @@ func GetQuestionList() ([]*proto.QuestionList, error) {
 	for i := 1; i < 5; i++ {
 		var info []*proto.QuestionList
 		switch i {
-		case 1: limit = 15
+		case 1: limit = 10
 		case 2: limit = 10
 		case 3: limit = 10
-		case 4: limit = 5
+		case 4: limit = 10
 		}
 		err := db.Table("question").
 			Select("`num1`, `operator`, `num2`, `result`, `success`, `seconds`").
-			Where("`level` = ?", i).
+			Where("`level` = ?", 4).
 			Limit(limit).Order("rand()").Find(&info).Error
 		if err != nil {
 			return nil, err
